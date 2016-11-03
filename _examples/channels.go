21e7ff02878842cba9a49380d714a6a9f37284a2
Takefumi MIYOSHI <miyo@wasamon.net>
2016-11-03 09:56:59 +0900

    add clean.sh

--- _examples/channels.go
+++ _examples/channels.go
@@ -4,6 +4,14 @@ import "fmt"
 
 var c chan int = make(chan int)
 	
+func sum(s []int, c chan int){
+	sum := 0
+	for _,v := range s{
+		sum += v
+	}
+	c <- sum
+}
+
 func main(){
 	s := []int{7,2,8,-9,4,0}
 
---
+++ clean.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm -f *.ir
+rm -f *.dot
+rm -f *scheduler*.txt
+rm -f *.o
+rm -f *.vhd
+rm -f *.v
+rm -f work-obj93.cf
